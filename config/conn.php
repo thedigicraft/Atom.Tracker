8bdc3cef8e2ee0f38515d8d781da64c988552035
The Digital Craft <alan@rtistics.com>
2015-08-23 15:47:28 -0400

    Comment added

--- config/conn.php
+++ config/conn.php
@@ -1,4 +1,5 @@
 <?
 # Connect to database:
+// You will need to change this to match your database info!
 $dbc = mysqli_connect('localhost','tracker','website','tracker') or die(mysqli_connect_error());
 ?>
